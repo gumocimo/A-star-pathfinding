6d0c42ac4953f210cc8df8e8a7e5bfba3c4daa81
gumocimo <guglielmo.cimolai@icloud.com>
2025-08-20 17:01:56 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 """A* Pathfinding Algorithm Package"""
 
-from .core import Node, AStar, MazeGenerator
+from .core import Node, AlgorithmState, AStar, MazeGenerator
 from .visualization import ConsoleVisualizer, MatplotlibVisualizer
 
 __all__ = [
     'Node',
+    'AlgorithmState',
     'AStar',
     'MazeGenerator',
     'ConsoleVisualizer',
